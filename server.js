faad60737021003d67ee5968c5402d9b57ff5083
cthompsondev <cthompson.development@gmail.com>
2019-11-21 13:32:46 -0600

    start of project

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const session = require('express-session');
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/serfsandsultans', {useNewUrlParser: true});
+
+const server = app.listen(8000, () => console.log("still listening on port 8000"));
+const io = require('socket.io')(server);
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}) );
+app.use(bodyParser.json())
+
+require('./server/config/routes.js')(app)
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/SerfsSultans/dist/SerfsSultans"));
+app.set('views', __dirname + '/views');
+
+
+app.use(session({
+  secret: 'I Solemly swear i am upto no good!',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}))
+
+
+var counter = 0;
+    
+io.on('connection', function (socket) { 
+  // socket.on('color',() => io.emit('color',{color:'red'}))
+  // console.log('someone connected')
+  // socket.emit('greeting', { msg: 'Greetings, from server Node, brought to you by Sockets! -Server' }); //3
+  // socket.on('thankyou', function (data) { 
+  //   console.log(data.msg); 
+  // });
+    
+});
+
+
+
+
+
+
+
+
---
+++ server/config/routes.js
@@ -0,0 +1,57 @@
+
+
+    dogs = require('../controllers/dogs.js')
+
+
+module.exports = function(app){
+
+    // app.get('/initdogs', (req, res) => {
+    //     const dog = new Dog();
+    //     dog.name = 'Majestic Dog';
+    //     dog.url='./assets/majesticdog.jpg';
+    //     dog.favoriteToy = 'Hedgey';
+    //     dog.save()
+    //     res.redirect('/')
+    // })
+
+    // app.get('/dogs', (req,res)=>{
+    //     console.log('getting dogs')
+    //     Dog.find({})
+    //     .then(dogs => {res.json(dogs)})
+    //     .catch(err => {
+    //         console.log('stop making errs:',err);
+    //         res.redirect('/')
+    //     })
+
+    // })
+
+    // app.post('/dogs', dogs.create)
+
+    // app.get('/dungeon', function (req, res) {
+       
+    //     res.redirect('/')
+       
+    // })
+       
+    // app.get('/castle', function (req, res) {
+       
+    //     res.redirect('/')
+       
+    // })
+
+    // app.get('/bob', (req,res)=>{
+    //     users.create(req,res)
+    //     res.redirect('/')
+    // })
+    // app.get('/bobs', (req,res)=>{
+    //     users.find({name: 'bob'})
+    //     .then(data =>{
+    //         res.setHeader('Content-Type', 'application/json');
+    //         res.end(JSON.stringify(data));
+
+    //     })
+    //     .catch(err => {
+    //         console.log('err:',err)
+    //     })
+    // })
+}   
